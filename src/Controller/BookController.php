42a5fe66683150bcbdd9915c024c228efff8a22f
Fercak Florian <fercak.florian@gmail.com>
2026-04-04 18:44:09 +0200

    access to book list enabled for users

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller;
 
 use App\Entity\Book;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Pagerfanta\Doctrine\ORM\QueryAdapter;
-use Pagerfanta\Pagerfanta;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/admin/book')]
+#[Route('/book')]
 class BookController extends AbstractController
 {
-    #[Route('', name: 'app_admin_book_index', methods: ['GET'])]
+    #[Route('', name: 'app_book_index', methods: ['GET'])]
     public function index(Request $request, BookRepository $repository): Response
     {
         $books = Pagerfanta::createForCurrentPageWithMaxPerPage(
@@ -26,22 +26,22 @@ class BookController extends AbstractController
             4 
         );
 
-        return $this->render('admin/book/index.html.twig', [
+        return $this->render('book/index.html.twig', [
             'books' => $books,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    #[Route('/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function show(?Book $book /*BookRepository $repository*/): Response
     {
         // $book = $repository->find(['id' => $id]);
-        return $this->render('admin/book/show.html.twig', [
+        return $this->render('book/show.html.twig', [
             'book' => $book
         ]);
     }
     #[IsGranted('ROLE_AJOUT_DE_LIVRE')]
-    #[Route('/{id}/edit', name: 'app_admin_book_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_book_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_book_new', methods: ['GET', 'POST'])]
     public function new(?Book $book, Request $request, EntityManagerInterface $manager): Response
     {
         if ($book) {
@@ -57,10 +57,10 @@ class BookController extends AbstractController
             $manager->persist($book);
             $manager->flush();
 
-            return $this->redirectToRoute('app_admin_book_show', ['id' => $book->getId()]);
+            return $this->redirectToRoute('app_book_show', ['id' => $book->getId()]);
         }
         
-        return $this->render('admin/book/new.html.twig', [
+        return $this->render('book/new.html.twig', [
             'form' => $form
         ]);
     }
