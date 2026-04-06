85f7ff5144d9796b8c440ee8a23dc912ac62e9d8
Fercak Florian <fercak.florian@gmail.com>
2026-04-06 18:40:05 +0200

    access to book list enabled for unauthenticated users

---
+++ src/Controller/BookController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookRepository;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/book')]
+class BookController extends AbstractController
+{
+    #[Route('', name: 'app_book_index', methods: ['GET'])]
+    public function index(Request $request, BookRepository $repository): Response
+    {
+        $books = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($repository->createQueryBuilder('a')),
+            $request->query->get('page', 1),
+            4
+        );
+
+        return $this->render('book/index.html.twig', [
+            'books' => $books,
+        ]);
+    }
+}
